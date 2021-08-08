0ce0a61143b47feea1e89797d8dad6e7602beecb
yoshiyasugimoto <gabesaiti@gmail.com>
2021-08-08 18:58:25 +0900

    delete .vscode

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
   @Get()
   findAll(): string {
     return 'This action returns all cats';
